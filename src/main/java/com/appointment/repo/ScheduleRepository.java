1498c95a7f985a2a2338dc9aebfe46a17ecf5c1f
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-06-25 13:00:10 +0530

    Update ScheduleRepository.java

--- src/main/java/com/appointment/repo/ScheduleRepository.java
+++ src/main/java/com/appointment/repo/ScheduleRepository.java
@@ -10,7 +10,7 @@ import com.appointment.entity.Schedule;
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-	// write your code for dynamic query method to find an return a list of schedules for the specified doctor on the specified day
+	// write your code for dynamic query method to find and return a list of schedules for the specified doctor on the specified day
 
 	
 }
